655620594983d085009dc3e2362f0e5b71d4ffef
Eyal Azulay <eyal.git@tiyal.com>
2020-01-02 10:33:50 +0000

    Changed date format (month)

--- billing_inc.php
+++ billing_inc.php
@@ -181,7 +181,7 @@
 		if ($f_export_bug_stats_button){
 			ob_start();
 			echo "\"Period: {$t_bugnote_stats_from_d}/{$t_bugnote_stats_from_m}/{$t_bugnote_stats_from_y} - {$t_bugnote_stats_to_d}/{$t_bugnote_stats_to_m}/{$t_bugnote_stats_to_y}\"".
-				$t_sep.$t_sep.$t_sep.'"'.gmdate( 'd/M/Y', time() ).'"'.$t_nl;
+				$t_sep.$t_sep.$t_sep.'"'.gmdate( 'd/m/Y', time() ).'"'.$t_nl;
 			echo "Project{$t_sep}ID{$t_sep}Summary{$t_sep}Current Status{$t_sep}Time Spent (minutes)".$t_nl;
 		}else{
 ?>
