0ad8da5fdd85514b214248813ac56e5de514ae0e
Eyal Azulay <eyal.git@tiyal.com>
2020-01-07 16:42:38 +0000

    Add similar warning if following a link

--- javascript/min/form_warnings.js
+++ javascript/min/form_warnings.js
@@ -5,20 +5,37 @@ function setWarningOnNavigate() {
     forms = document.getElementsByTagName("form");
     for (var i = 0, formsLen = forms.length; i < formsLen; i++) {
         var form = forms[i];
-        var formName = form.name;
-        updatedForms[formName] = false;
+        updatedForms[i] = false;
         var fields = form.querySelectorAll('input[type="text"],textarea');
         for (var j = 0, fieldsLen = fields.length; j < fieldsLen; j++) {
             var field = fields[j];
             field.addEventListener('input', function () {
-                updatedForms[formName] = true;
+                updatedForms[i] = true;
             });
         }
         form.onsubmit = function () {
-            if (updatedForms[formName]) {
-                return confirm('You are going to lose your changes. Do you wish to proceed?');
+            if (updatedForms[i]) {
+                return confirm('You have not yet saved your previous changes. You will lose them if you proceed with the current action. Do you wish to proceed?');
             }
             return true;
         };
+    }
+    links = document.getElementsByTagName("a");
+    for (var i = 0, formsLen = forms.length; i < formsLen; i++) {
+        var link = links[i];
+        link.addEventListener('click', function (e) {
+            for (var j = 0, formsLen = updatedForms.length; j < formsLen; j++) {
+                var formUpdated = updatedForms[j];
+                if (formUpdated) {
+                    if (!confirm('You have not yet saved your previous changes. You will lose them if you proceed with the current action. Do you wish to proceed?')) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        return false;
+                    } else
+                        return true;
+                }
+            }
+            return true;
+        });
     }
 }
