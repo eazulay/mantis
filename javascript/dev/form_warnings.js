63c074937ed33414e908771fb91e0b12b41735cd
Eyal Azulay <eyal.git@tiyal.com>
2020-01-07 16:04:42 +0000

    Check array length first

--- javascript/dev/form_warnings.js
+++ javascript/dev/form_warnings.js
@@ -1,21 +1,23 @@
 var forms = [];
 var updatedForms = [];
 
-function setWarningOnNavigate(){
+function setWarningOnNavigate() {
     forms = document.getElementsByTagName("form");
-    for(var formKey in forms){
+    for (var formKey in forms) {
         var form = forms[formKey];
         var formName = form.name;
         updatedForms[formName] = false;
         var fields = form.querySelectorAll('input[type="text"],textarea');
-        for(var fieldKey in fields){
-            var field = fields[fieldKey];
-            field.addEventListener('input', function(){
-                updatedForms[formName] = true;
-            });
+        if (fields.length > 0) {
+            for (var fieldKey in fields) {
+                var field = fields[fieldKey];
+                field.addEventListener('input', function () {
+                    updatedForms[formName] = true;
+                });
+            }
         }
-        form.addEventListener('submit', function(){
-            if (updatedForms[formName]){
+        form.addEventListener('submit', function () {
+            if (updatedForms[formName]) {
                 return confirm('You are going to lose your changes. Do you wish to proceed?');
             }
             return true;
