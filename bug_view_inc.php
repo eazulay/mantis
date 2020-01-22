c090adbee1b29781b1daf565b90a817bdd52a8dc
Eyal Azulay <eyal.git@tiyal.com>
2020-01-22 12:48:48 +0000

    Render Attachments collapsed
    Render Attachments collapsed or not depending on the (existing) cookie.

--- bug_view_inc.php
+++ bug_view_inc.php
@@ -608,12 +608,13 @@
 	if ( $tpl_show_attachments ) {
 		echo '<tr ', helper_alternate_class(), '>';
 		echo '<td class="category" colspan="2">';
-		echo '<a href="" onclick="if (ToggleDiv( \'attachments\' )) this.firstChild.src=\'images/minus.png\';
-								  else this.firstChild.src=\'images/plus.png\';
-								  return false;"><img src="images/minus.png" alt="-" border="0" /></a> ';
+		$attachmentsCollapsed = collapse_display('attachments');
+		echo '<a href="" onclick="if (ToggleDiv(\'attachments\')) this.firstChild.src=\'images/minus.png\';
+								else this.firstChild.src=\'images/plus.png\';
+								return false;"><img src="images/' . ($attachmentsCollapsed ? 'plus.png' : 'minus.png') . '" alt="-" border="0" /></a> ';
 		echo lang_get( 'attached_files' ), '</td>';
 		echo '<td colspan="8">';
-		echo '<div id="attachments_open">';
+		echo '<div id="attachments_open" class="' . ($attachmentsCollapsed ? 'hidden' : '') . '">';
 		# File upload
 		if ( $tpl_show_upload_form && file_allow_bug_upload( $f_bug_id ) ) {
 			$t_max_file_size = (int)min( ini_get_number( 'upload_max_filesize' ), ini_get_number( 'post_max_size' ), config_get( 'max_file_size' ) );
@@ -628,8 +629,7 @@
 		}
 		print_bug_attachments_list( $tpl_bug_id );
 		echo '</div>';
-		echo '<div id="attachments_closed" class="hidden">';
-		//echo '<a href="" onclick="ToggleDiv( \'attachments\' ); return false;"><img src="images/plus.png" alt="+" border="0" /></a> ';
+		echo '<div id="attachments_closed" class="' . ($attachmentsCollapsed ? '' : 'hidden') . '">';
 		# File upload
 		if ( $tpl_show_upload_form && file_allow_bug_upload( $f_bug_id ) ) {
 			$t_max_file_size = (int)min( ini_get_number( 'upload_max_filesize' ), ini_get_number( 'post_max_size' ), config_get( 'max_file_size' ) );
