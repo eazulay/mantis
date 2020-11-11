aa1a0d981b56604b1e7b599beef7e86211596780
Eyal Azulay <eyal.git@tiyal.com>
2020-11-11 21:00:58 +0000

    Prevent expansion of header

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -63,13 +63,13 @@ $num_notes = count( $t_bugnotes );
 
 <table class="width100" cellspacing="0">
 <tr class="header">
-	<td class="form-title" colspan="2">
+	<td class="form-title" colspan="2" style="position:relative;">
 <?php
     collapse_icon( 'bugnotes' );
     echo lang_get( 'bug_notes_title' );
     if (access_has_global_level(DEVELOPER)){ ?>
     &nbsp; &nbsp;
-    <form id="bugnotes_move" method="post" action="bugnotes_move.php?bug_id=<?php echo $f_bug_id; ?>" style="display:none; height:13px;">
+    <form id="bugnotes_move" method="post" action="bugnotes_move.php?bug_id=<?php echo $f_bug_id; ?>" style="display:none; position:absolute; top:3px;">
     <?php echo form_security_field( 'bugnotes_move' ); ?>
         <label for="move_to_bug_id">Move Notes to Issue #</label> <input type="text" id="move_to_bug_id" name="move_to_bug_id" size="9">
         <input type="checkbox" id="record_transfer" name="record_transfer" value="1" checked> <label for="record_transfer">Record transfer on original Issue</label>
