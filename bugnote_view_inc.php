66297ea8e2695940819861c1fec7ea40cd270ffc
Eyal Azulay <eyal.git@tiyal.com>
2022-02-04 17:06:02 +0100

    Hide the Make Private/Public button
    Hide if you're not allowed to see Private notes.

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -187,8 +187,7 @@ $num_notes = count( $t_bugnotes );
 			}
 
 			# users with access to both update and change view status (or the bugnote author) can change public/private status
-			if ( $t_can_edit_note || ( access_has_bug_level( config_get( 'update_bugnote_threshold' ), $f_bug_id ) &&
-				access_has_bug_level( config_get( 'change_view_status_threshold' ), $f_bug_id ) ) ) {
+			if ( access_has_bug_level( config_get( 'private_bugnote_threshold' ), $t_bugnote->id ) ) {
 				if ( VS_PRIVATE == $t_bugnote->view_state ) {
 					echo " ";
 					print_button( 'bugnote_set_view_state.php?private=0&bugnote_id=' . $t_bugnote->id, lang_get( 'make_public' ) );
