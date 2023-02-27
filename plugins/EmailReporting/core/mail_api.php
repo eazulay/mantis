d75d1a2842115f59071342960bafa1b8d6eb39e4
Eyal Azulay <eyal.git@tiyal.com>
2023-02-27 13:04:01 +0100

    Moved the debug even earlier

--- plugins/EmailReporting/core/mail_api.php
+++ plugins/EmailReporting/core/mail_api.php
@@ -495,6 +495,7 @@ class ERP_mailbox_api
 		$this->_mails_fetched++;
 
 		$this->save_message_to_file( 'raw_msg', $t_msg );
+        var_dump($t_msg);
 
 		$t_email = $this->parse_content( $t_msg );
 
@@ -553,7 +554,6 @@ class ERP_mailbox_api
 	# parse the email using mimeDecode for Mantis
 	private function parse_content( &$p_msg )
 	{
-        var_dump($p_msg);
 		$this->show_memory_usage( 'Start Mail Parser' );
 
 		$t_mp = new ERP_Mail_Parser( $this->_mp_options );
