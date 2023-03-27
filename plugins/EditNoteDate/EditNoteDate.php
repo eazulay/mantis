a63acb91498b1f1d7f49845e851c97e823fa4069
Eyal Azulay <eyal.git@tiyal.com>
2023-03-27 18:28:04 +0200

    Undo Timezone adjustment
    The new server is set to UTC.

--- plugins/EditNoteDate/EditNoteDate.php
+++ plugins/EditNoteDate/EditNoteDate.php
@@ -64,8 +64,8 @@ class EditNoteDatePlugin extends MantisPlugin {
 				$month = $date[mon];
 				$day = $date[mday];
 				$wday = $date[wday];
-				if ($month == 3 && $day-$wday >=25 || $month > 3 && $month < 10 || $month == 10 && $day-$wday < 25)
-					$tz_offset -= 3600;
+//				if ($month == 3 && $day-$wday >=25 || $month > 3 && $month < 10 || $month == 10 && $day-$wday < 25)
+//					$tz_offset -= 3600;
 				$t_query = 'UPDATE mantis_bugnote_table SET date_submitted = unix_timestamp(' . db_param() . ') - '.$tz_offset.' WHERE id=' . db_param();
 				db_query_bound($t_query, array($date_submitted, $p_bugnote_id));
 			}
