a837e7fb075cd179c05fc2daa53743a917f37881
Eyal Azulay <eyal.git@tiyal.com>
2025-01-08 20:23:55 +0100

    Added a colon

--- bugnote_view_inc.php
+++ bugnote_view_inc.php
@@ -201,7 +201,7 @@ $num_notes = count( $t_bugnotes );
 			echo ' <input type="button" class="button-small" onclick="copyNote('.$t_bugnote->id.');" value="Copy" />
 		<div class="copy-options hidden">
 			<form method="post" action="bugnote_add.php">
-				<input type="hidden" name="bugnote_text" value="Duplicate of ~'.$t_bugnote->id."\n".addslashes($t_bugnote->note).'" />
+				<input type="hidden" name="bugnote_text" value="Duplicate of ~'.$t_bugnote->id.":\n".addslashes($t_bugnote->note).'" />
 				<Label>To: <input type="number" name="bug_id" min="1" value="'.$f_bug_id.'" /></Label>
 				<input type="submit" class="button-small" onclick="copyNoteOverride(event,'.$t_bugnote->id.','.$f_bug_id.');" value="Apply" />
 			</form>
