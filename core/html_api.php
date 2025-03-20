1d5aba7b0763bf18abd5a1cdb035593cab935895
Eyal Azulay <eyal.git@tiyal.com>
2025-03-20 15:34:31 +0100

    Show Change Log and Roadmap menus only for Managers

--- core/html_api.php
+++ core/html_api.php
@@ -748,14 +748,16 @@ function print_menu() {
 			$t_menu_options[] = string_get_bug_report_link();
 		}
 
-		# Changelog Page
-		if( access_has_project_level( config_get( 'view_changelog_threshold' ) ) ) {
-			$t_admin_menu_options[] = '<a href="' . helper_mantis_url( 'changelog_page.php">' ) . lang_get( 'changelog_link' ) . '</a>';
-		}
+		if( access_has_global_level( MANAGER ) ) {
+			# Changelog Page
+			if( access_has_project_level( config_get( 'view_changelog_threshold' ) ) ) {
+				$t_admin_menu_options[] = '<a href="' . helper_mantis_url( 'changelog_page.php">' ) . lang_get( 'changelog_link' ) . '</a>';
+			}
 
-		# Roadmap Page
-		if( access_has_project_level( config_get( 'roadmap_view_threshold' ) ) ) {
-			$t_admin_menu_options[] = '<a href="' . helper_mantis_url( 'roadmap_page.php">' ) . lang_get( 'roadmap_link' ) . '</a>';
+			# Roadmap Page
+			if( access_has_project_level( config_get( 'roadmap_view_threshold' ) ) ) {
+				$t_admin_menu_options[] = '<a href="' . helper_mantis_url( 'roadmap_page.php">' ) . lang_get( 'roadmap_link' ) . '</a>';
+			}
 		}
 
 		# Summary Page
