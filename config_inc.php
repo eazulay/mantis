0ea42cb9646cfac7b2634b7b7fe86d216c7d36e8
Eyal Azulay <eyal.git@tiyal.com>
2025-04-10 11:46:50 +0200

    Decreased h1,h2 sizes and made them less bold
    All this only in Markdown text.

--- config_inc.php
+++ config_inc.php
@@ -240,6 +240,6 @@ $g_my_view_boxes = array (
 $g_show_project_menu_bar = ON;
 $g_form_security_validation = OFF;
 
-$g_css_include_file = 'css/default.css?v=20250409';
+$g_css_include_file = 'css/default.css?v=20250410';
 
 // $g_relationship_graph_enable = ON;
